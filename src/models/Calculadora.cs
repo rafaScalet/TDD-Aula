70831f1baeaf0df610f590bdf23257f5da3e523d
rafaScalet <rafaelscalet2018@gmail.com>
2024-05-07 23:59:49 -0300

    functions/methods

--- src/models/Calculadora.cs
+++ src/models/Calculadora.cs
@@ -7,6 +7,29 @@ namespace src.models
 {
     public class Calculadora
     {
-        
+        public int somar(int num1, int num2)
+        {
+            return 0;
+        }
+
+        public int subtrair(int num1, int num2)
+        {
+            return 0;
+        }
+
+        public int multiplicar(int num1, int num2)
+        {
+            return 0;
+        }
+
+        public int dividir(int num1, int num2)
+        {
+            return 0;
+        }
+
+        public List<string> historico()
+        {
+            return new List<string>();
+        }
     }
 }
